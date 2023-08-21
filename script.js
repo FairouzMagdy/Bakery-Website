84d7ab63cd62bc18a03962505ee9e6dabd8e2c6c
Fairouz Magdy <91697747+FairouzMagdy@users.noreply.github.com>
2023-08-21 23:33:41 +0300

    Add files via upload

---
+++ script.js
@@ -0,0 +1,10 @@
+// const section1 = document.querySelector("#intro");
+// const nav = document.querySelector("nav");
+
+// const initialCoords = section1.getBoundingClientRect();
+// console.log(initialCoords);
+
+// window.addEventListener("scroll", function (e) {
+//   if (window.scrollY > initialCoords.top) nav.classList.add("sticky");
+//   else nav.classList.remove("sticky");
+// });
